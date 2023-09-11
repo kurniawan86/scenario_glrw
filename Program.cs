0083abdcd89c920021719a062fa33967a80b3b85
muchamadkurniawan <kernel.journal@itats.ac.id>
2023-09-11 22:23:45 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace scenario_glrw
+{
+    class ScenarioGLRW
+    {
+        public ScenarioGLRW()
+        {
+            Console.WriteLine("tes constructore");
+        }
+        
+        static void Main()
+        {
+            ScenarioGLRW s = new ScenarioGLRW();
+            Scenario1 gl = new Scenario1();
+            gl.readExcelWellTrajectory();
+        }
+    }
+}
+
